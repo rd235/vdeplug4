5f05076a928385a570c95c8b98c9a8edfbd258dc
Renzo Davoli <renzo@cs.unibo.it>
2019-11-22 15:05:14 +0100

    new search sequence for plugins

--- libvdeplug4/libvdeplug.c
+++ libvdeplug4/libvdeplug.c
@@ -34,21 +34,31 @@
 /* Per-User standard switch definition */
 /* This will be prefixed by getenv("HOME") */
 /* it can be a symbolic link to the switch dir */
-#define STDSWITCH "/.vde2/default.switch"
+#define STDSWITCH "/.vdeplug/default"
+#define OLDSTDSWITCH "/.vde2/default.switch"
 
-#define DEFAULT_MODULE "libvdeplug_vde.so"
+#define STD_LIB_PATH "/lib:/usr/lib"
+#define LIBVDEPLUG "libvdeplug_"
+#define LIBVDEPLUGIN "/vdeplug/" LIBVDEPLUG
 
-VDECONN *vde_open_module(char *modname, char *vde_url, char *descr,int interface_version,
-		    struct vde_open_args *open_args) {
-	void *handle=dlopen(modname, RTLD_NOW | RTLD_DEEPBIND);
+#define DEFAULT_MODULE "vde"
+uint64_t __vde_version_tag = 4;
 
+struct vde_open_parms {
+	char *vde_args;
+	char *descr;
+	int interface_version;
+	struct vde_open_args *open_args;
+};
+
+static VDECONN *vde_open_module_lib(char *filename, struct vde_open_parms *parms) {
+	void *handle=dlopen(filename, RTLD_NOW | RTLD_DEEPBIND);
+
+	//printf("TRY %s %s\n", filename, parms->vde_args);
 	if (handle) {
-		struct vdeplug_module *module=dlsym(handle,"vdeplug_ops");
+		struct vdeplug_module *module=dlsym(handle, "vdeplug_ops");
 		if (module) {
-			size_t vde_url_len = strlen(vde_url);
-			char vde_url_copy[vde_url_len+1];
-			strcpy(vde_url_copy, vde_url);
-			VDECONN *rv = module->vde_open_real(vde_url_copy, descr, interface_version, open_args);
+			VDECONN *rv = module->vde_open_real(parms->vde_args, parms->descr, parms->interface_version, parms->open_args);
 			if (rv) {
 				rv->handle = handle;
 				rv->module = module;
@@ -62,73 +72,167 @@ VDECONN *vde_open_module(char *modname, char *vde_url, char *descr,int interface
 	return NULL;
 }
 
-VDECONN *vde_open_real(char *given_vde_url, char *descr,int interface_version,
-		    struct vde_open_args *open_args)
+static VDECONN *vde_open_samedir(char *modname, char *subdir, struct vde_open_parms *parms) {
+	char path[PATH_MAX];
+	Dl_info info;
+	if (dladdr(&__vde_version_tag, &info) != 0) {
+		const char *libpath = info.dli_fname;
+		char *slash = strrchr(libpath, '/');
+		if (slash) {
+			int len = slash - libpath;
+			snprintf(path, PATH_MAX, "%*.*s%s%s.so",
+					len, len, libpath, subdir, modname);
+			return vde_open_module_lib(path, parms);
+		}
+	}
+	return errno = EPROTONOSUPPORT, NULL;
+}
+
+static VDECONN *vde_open_dirlist(char *modname, char *dirlist, char *subdir, struct vde_open_parms *parms) {
+	char path[PATH_MAX];
+	VDECONN *retval;
+	if (dirlist) {
+		while (*dirlist != 0) {
+			int len = strchrnul(dirlist, ':') - dirlist;
+			snprintf(path, PATH_MAX, "%*.*s%s%s.so",
+					len, len, dirlist, subdir, modname);
+			retval = vde_open_module_lib(path, parms);
+			if (retval != NULL || errno != EPROTONOSUPPORT)
+				return retval;
+			dirlist += len + (dirlist[len] == ':');
+		}
+	}
+	return errno = EPROTONOSUPPORT, NULL;
+}
+
+static VDECONN *vde_open_default(char *modname, struct vde_open_parms *parms) {
+	char path[PATH_MAX];
+	snprintf(path, PATH_MAX, LIBVDEPLUG "%s.so", modname);
+	return vde_open_module_lib(path, parms);
+}
+
+static VDECONN *vde_open_module(char *modname, struct vde_open_parms *parms) {
+	char *vdeplugin_path = getenv("VDEPLUGIN_PATH");
+	// creata a copy of parms->vde_args
+	size_t vde_args_len = strlen(parms->vde_args);
+	char vde_args_copy[vde_args_len + 1];
+	strcpy(vde_args_copy, parms->vde_args);
+	parms->vde_args = vde_args_copy;
+
+	// if VDEPLUGIN_PATH exists it defines the search path for plugins
+	if (vdeplugin_path)
+		return vde_open_dirlist(modname, vdeplugin_path, "/" LIBVDEPLUG, parms);
+	else {
+		VDECONN *retval;
+		retval = vde_open_samedir(modname, LIBVDEPLUGIN, parms);
+		if (retval != NULL || errno != EPROTONOSUPPORT) return retval;
+		retval = vde_open_samedir(modname, "/" LIBVDEPLUG, parms);
+		if (retval != NULL || errno != EPROTONOSUPPORT) return retval;
+		retval = vde_open_dirlist(modname, getenv("LD_LIBRARY_PATH"), LIBVDEPLUGIN, parms);
+		if (retval != NULL || errno != EPROTONOSUPPORT) return retval;
+		retval = vde_open_dirlist(modname, STD_LIB_PATH, LIBVDEPLUGIN, parms);
+		if (retval != NULL || errno != EPROTONOSUPPORT) return retval;
+		return vde_open_default(modname, parms);
+	}
+}
+
+static inline size_t getpw_bufsize(void) {
+	size_t bufsize = sysconf(_SC_GETPW_R_SIZE_MAX);
+	return bufsize > 0 ? bufsize : PATH_MAX;
+}
+
+static void set_newdescr(const char *descr, char *newdescr, size_t newdescrlen) {
+	char *ssh_client = getenv("SSH_CLIENT");
+	int pid = getpid();
+	uid_t uid = geteuid();
+	struct passwd pwd;
+	struct passwd *callerpwd;
+	size_t bufsize = getpw_bufsize();
+	char buf[bufsize];
+	FILE *f = fmemopen(newdescr, newdescrlen, "w");
+
+	fprintf(f, "%s ", descr);
+
+	getpwuid_r(uid, &pwd, buf, bufsize, &callerpwd);
+	if (callerpwd)
+		fprintf(f, "user=%s", callerpwd->pw_name);
+	else
+		fprintf(f, "user=%d", uid);
+	fprintf(f, " pid=%d", pid);
+
+	if (ssh_client) {
+		int len = strchrnul(ssh_client,' ') - ssh_client;
+		fprintf(f, " SSH=%*.*s", len, len, ssh_client);
+	}
+
+	fclose(f);
+	newdescr[newdescrlen - 1] = 0;
+	//printf("%s %d\n", newdescr, bufsize);
+}
+
+VDECONN *vde_open_real(char *vde_url, char *descr, int interface_version,
+		struct vde_open_args *open_args)
 {
 	char std_vde_url[PATH_MAX];
 	struct stat statbuf;
 	char newdescr[MAXDESCR];
-	int descrlen;
-	struct passwd *callerpwd;
-	char *ssh_client = getenv("SSH_CLIENT");
-	int pid = getpid();
 	char *tag;
+	struct vde_open_parms parms = {
+		.descr = newdescr,
+		.interface_version = interface_version,
+		.open_args = open_args
+	};
 
-	if (given_vde_url == NULL)
-		given_vde_url = "";
+	if (vde_url == NULL)
+		vde_url = "";
 
-	callerpwd=getpwuid(getuid());
+	tag = strstr(vde_url, "://");
 
-	descrlen=snprintf(newdescr,MAXDESCR,"%s user=%s PID=%d",
-			descr,(callerpwd != NULL)?callerpwd->pw_name:"??",
-			pid);
+	set_newdescr(descr, newdescr, MAXDESCR);
 
-	if (ssh_client) {
-		char *endofip=strchr(ssh_client,' ');
-		if (endofip) *endofip=0;
-		descrlen+=snprintf(newdescr+descrlen,MAXDESCR-descrlen,
-				" SSH=%s", ssh_client);
-		if (endofip) *endofip=' ';
-	}
-
-	if (*given_vde_url == '\0') {
-		char *homedir = getenv("HOME");
-		if (homedir) {
-			char *stdswitch;
-			if (asprintf(&stdswitch, "%s%s", homedir, STDSWITCH) >= 0) {
-				if (readlink(stdswitch,std_vde_url,PATH_MAX) >= 0)
-					given_vde_url=std_vde_url;
-				free(stdswitch);
-			}
-		}
-	}
-	if (lstat(given_vde_url,&statbuf) >= 0) {
-		if (S_ISREG(statbuf.st_mode)) {
-			FILE *f=fopen(given_vde_url,"r");
-			if (f != NULL) {
-				if (fgets(std_vde_url,PATH_MAX,f) != NULL) {
-					std_vde_url[strlen(std_vde_url)-1] = 0;
-					given_vde_url=std_vde_url;
+	if (tag == NULL) {
+		if (*vde_url == '\0') {
+			char *homedir = getenv("HOME");
+			if (homedir) {
+				snprintf(std_vde_url, PATH_MAX, "%s%s", homedir, STDSWITCH);
+				if (lstat(std_vde_url, &statbuf) >= 0)
+					vde_url = std_vde_url;
+				else {
+					snprintf(std_vde_url, PATH_MAX, "%s%s", homedir, OLDSTDSWITCH);
+					if (lstat(std_vde_url, &statbuf) >= 0)
+						vde_url = std_vde_url;
 				}
-				fclose(f);
+			}
+		}
+		if (stat(vde_url, &statbuf) >= 0) {
+			if (S_ISREG(statbuf.st_mode)) {
+				FILE *f=fopen(vde_url,"r");
+				if (f != NULL) {
+					if (fgets(std_vde_url, PATH_MAX, f) != NULL) {
+						std_vde_url[strlen(std_vde_url) - 1] = 0;
+						vde_url = std_vde_url;
+					}
+					tag = strstr(vde_url, "://");
+					fclose(f);
+				}
 			}
 		}
 	}
-	if (given_vde_url == NULL || (tag=strstr(given_vde_url,"://")) == NULL) {
-		return vde_open_module(DEFAULT_MODULE, given_vde_url, newdescr,
-				interface_version, open_args);
+	if (vde_url == NULL || tag == NULL) {
+		parms.vde_args = vde_url;
+		return vde_open_module(DEFAULT_MODULE, &parms);
 	} else {
-		int modlen=tag-given_vde_url;
-		char modname[modlen+15];
-		snprintf(modname, modlen+15, "libvdeplug_%*.*s.so", modlen, modlen, given_vde_url);
-		return vde_open_module(modname,given_vde_url+(modlen+3),newdescr,
-				                                            interface_version, open_args);
+		int modlen=tag - vde_url;
+		char modname[modlen + 1];
+		snprintf(modname, modlen + 1, "%*.*s", modlen, modlen, vde_url);
+		parms.vde_args = vde_url + (modlen + 3);
+		return vde_open_module(modname, &parms);
 	}
 }
 
 ssize_t vde_recv(VDECONN *conn,void *buf,size_t len,int flags)
 {
-	if (__builtin_expect(conn!=0,1)) 
+	if (__builtin_expect(conn!=0,1))
 		return conn->module->vde_recv(conn,buf,len,flags);
 	else {
 		errno=EBADF;
@@ -138,7 +242,7 @@ ssize_t vde_recv(VDECONN *conn,void *buf,size_t len,int flags)
 
 ssize_t vde_send(VDECONN *conn,const void *buf,size_t len,int flags)
 {
-	if (__builtin_expect(conn!=0,1)) 
+	if (__builtin_expect(conn!=0,1))
 		return conn->module->vde_send(conn,buf,len,flags);
 	else {
 		errno=EBADF;
